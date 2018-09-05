26dacd21bd22cdd7250dd7100f835a99ace0dea5
Chen Dar <cdoar@vmware.com>
2018-09-05 08:04:01 -0700

    install addional dependencies

--- setup.sh
+++ setup.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 #install for fzf, to find hidden files
-sudo apt-get install -y silversearcher-ag
+sudo apt-get install silversearcher-ag -y
 
 #install for YouCompleteMe
 sudo apt-get install build-essential cmake -y
-sudo apt-get install python-dev python3-dev
+sudo apt-get install python-dev python3-dev -y
+
+#install starscope
+sudo apt-get install ruby ruby-dev -y
+sudo gem install -V starscope
 
 #backup current .vimrc
 cp ~/.vimrc ~/.vimrc.old
@@ -13,3 +17,5 @@ cp ~/.vimrc ~/.vimrc.old
 #copy vimrc
 cp .vimrc ~/.vimrc
 
+mkdir -p ~/.vim/plugin/
+cp ./cscope/cscope_maps.vim ~/.vim/plugin
