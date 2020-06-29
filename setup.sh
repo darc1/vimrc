36acdf10957d93afe0bc3a6a07e7092e8c6b9d08
Chen Dar <chenjdar@gmail.com>
2020-06-29 17:07:44 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,21 +1,41 @@
 #!/bin/bash
 
-#install for fzf, to find hidden files
-sudo apt-get install silversearcher-ag -y
+#install pyenv
+curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
+#update .bashrc
+echo export PATH="/home/ubuntu/.pyenv/bin:$PATH" >> ~/.bashrc
+echo eval "$(pyenv init -)" >> ~/.bashrc
+echo "eval \"$(pyenv virtualenv-init -)\"" >> ~/.bashrc
+#update locally
+export PATH="/home/ubuntu/.pyenv/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
 
-#install for YouCompleteMe
-sudo apt-get install build-essential cmake -y
-sudo apt-get install python-dev python3-dev -y
+PY3_VER=3.8.3
+PY2_VER=2.7.18
+#configure python https://gist.github.com/klen/3d327e5f84e36dc04bd7
+env PYTHON_CONFIGURE_OPTS="--enable-shared" pyenv install -fk $PY3_VER
+env PYTHON_CONFIGURE_OPTS="--enable-shared" pyenv install -fk $PY2_VER
 
-#install starscope
-sudo apt-get install ruby ruby-dev -y
-sudo gem install -V starscope
+PY_CONFIG=`pyenv prefix $PY2_VER/lib/python2.7/config`
+PY3_PREFIX=`pyenv prefix $PY3_VER`
+PY3_CONFIG=`$PY3_PREFIX/bin/python-config --configdir`
 
-#backup current .vimrc
-cp ~/.vimrc ~/.vimrc.old
+mkdir -p ~/lib
+cd ~/lib
+ln -s ../.pyenv/versions/$PY2_VER/lib/libpython2.7.so
+ln -s ../.pyenv/versions/$PY3_VER/lib/libpython3.8.so
 
-#copy vimrc
-cp .vimrc ~/.vimrc
+#download vim code
+git clone https://github.com/vim/vim.git
+./configure                  \
+    --with-features=huge                    \
+    --enable-multibyte                      \
+    --enable-pythoninterp=dynamic           \
+    --enable-python3interp=dynamic          \
+    --with-python-config-dir=$PY_CONFIG     \
+    --with-python3-config-dir=$PY3_CONFIG   \
+    --enable-cscope                         \
+    --prefix=/usr
 
-mkdir -p ~/.vim/plugin/
-cp ./cscope/cscope_maps.vim ~/.vim/plugin
+
