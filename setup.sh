8ae5f8b23d747723cd463149a2691a960105b73d
Chen Dar <chenjdar@gmail.com>
2020-07-07 06:45:02 +0000

    fix vim compile on setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #
-# uninstall vim: sudo apt remove vim vim-runtime gvim
+
+#
+# to uninstall vim - sudo apt remove vim vim-runtime gvim
 #
 
 mkdir -p ~/tmp
@@ -26,42 +28,46 @@ else
 fi
 
 PY3_VER=3.8.3
-PY2_VER=2.7.18
+#PY2_VER=2.7.18
 #configure python https://gist.github.com/klen/3d327e5f84e36dc04bd7
 env PYTHON_CONFIGURE_OPTS="--enable-shared" pyenv install -k $PY3_VER
-env PYTHON_CONFIGURE_OPTS="--enable-shared" pyenv install -k $PY2_VER
+#env PYTHON_CONFIGURE_OPTS="--enable-shared" pyenv install -k $PY2_VER
 
-PY_CONFIG=`pyenv prefix $PY2_VER/lib/python2.7/config`
+#PY_CONFIG=`pyenv prefix $PY2_VER/lib/python2.7/config`
 PY3_PREFIX=`pyenv prefix $PY3_VER`
 PY3_CONFIG=`$PY3_PREFIX/bin/python-config --configdir`
 
 mkdir -p ~/lib
 cd ~/lib
-ln -s ../.pyenv/versions/$PY2_VER/lib/libpython2.7.so
-ln -s ../.pyenv/versions/$PY3_VER/lib/libpython3.8.so
+#ln -s ../.pyenv/versions/$PY2_VER/lib/libpython2.7.so
+#ln -s ../.pyenv/versions/$PY3_VER/lib/libpython3.8.so
 
-pyenv global $PY3_VER $PY2_VER
+pyenv global $PY3_VER
+#pyenv global $PY3_VER $PY2_VER
 
 #download vim code
 cd ~/tmp/
 git clone https://github.com/vim/vim.git
 cd vim/src
-./configure                  \
+#LDFLAGS="-L$HOME/.pyenv/versions/$PY3_VER/lib/" LIBS="-libpython3.8" ./configure                  \
+./configure --enable-fail-if-missing \
     --with-features=huge                    \
     --enable-multibyte                      \
-    --enable-pythoninterp=dynamic           \
     --enable-python3interp=dynamic          \
-    --with-python-config-dir=$PY_CONFIG     \
-    --with-python3-config-dir=$PY3_CONFIG   \
+    --with-python3-config-dir="$PY3_CONFIG"   \
     --enable-cscope                         \
     --prefix=/usr
 
-make
-sudo make install
+#make
+#sudo make install
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+if grep -R "alias vim" ~/.bashrc
+then
+  echo "vim alias already configured"
+else
+  #update .bashrc
+  echo alias vim=\"LD_LIBRARY_PATH=$PY3_PREFIX/lib vim\" >> ~/.bashrc
+  source ~/.bashrc
+fi
 
-nvm install node
+
